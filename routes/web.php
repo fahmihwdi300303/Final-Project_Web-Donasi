fc94ad29be15db3f43b95d678c633a37d0aa6570
fahmihwdi300303 <96535835+fahmihwdi300303@users.noreply.github.com>
2025-08-21 11:34:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,9 @@ Route::get('/', function () {
     return view('homepage');
 });
 
-Route::get('dashboard', function () {
-    return view = ('dashboard')
-})
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
 
 Route::get('/kegiatan', [App\Http\Controllers\KegiatanController::class, 'index']);
 
